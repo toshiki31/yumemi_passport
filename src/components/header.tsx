e4580540d5af5ec6a72455b3b9c02a2b5abe030f
mori toshiki <toshiki@morinoMacBook-Air.local>
2024-05-18 22:00:53 +0900

    refactor: scssの追加, app.tsxをpageに移動

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+  return <div className="header">都道府県の人口推移</div>
+}
+
+export default Header
---
+++ src/pages/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/app.scss'
+import { Header } from '../components/header'
+
+export default function App (){
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
